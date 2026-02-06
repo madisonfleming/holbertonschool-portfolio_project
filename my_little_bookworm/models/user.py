0ffea9a82d8be7254cc19a782f537935a0a3f186
Katherine Beringer <github@katbee.au>
2026-02-06 18:48:56 +1100

    feat: Extend User class to include name & email

--- my_little_bookworm/models/user.py
+++ my_little_bookworm/models/user.py
@@ -9,9 +9,35 @@ from models.base import Base
 
 
 class User(Base):
-    def __init__(self, role='standard'):
+    def __init__(self, name, email, role='standard'):
         super().__init__()
+        self.name = name
+        self.email = email
         self.role = role
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        if not name:
+            raise ValueError("Name must be provided")
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        self._name = name
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, email: str):
+        if not email:
+            raise ValueError("Email must be provided")
+        if not isinstance(email, str):
+            raise TypeError("Email must be a string")
+        self._email = email
 
     @property
     def role(self):
@@ -31,6 +57,9 @@ class User(Base):
     # TO-DO:
     # x validate role
     # x test validations
+    # x add all attributes in User class definition
+    #   nb - pw not required with Firebase
+    # Define methods
     # - updateUser(data)
     # - getLinkedChildren()
     # - getFamilyRoles()
