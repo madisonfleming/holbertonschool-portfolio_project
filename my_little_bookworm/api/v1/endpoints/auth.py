9c991e56fedd0084d960523681159cc74f7f712a
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-02-09 16:20:40 +1100

    -mfeat(auth): add firebase jwt token verification

---
+++ my_little_bookworm/api/v1/endpoints/auth.py
@@ -0,0 +1,24 @@
+# All protected endpoints will call this function to verify and decode the JWT token provided by Firebase
+# The function returns the decoded token to the calling endpoint OR raises an exception upon failure
+
+from firebase_admin import auth
+from firebase_admin.exceptions import FirebaseError
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+security = HTTPBearer() # extract Authorization header from http request
+
+def auth_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)) -> dict:
+    # credentials becomes a fastapi dependency object that stores scheme(Bearer) and credentials(token) at runtime 
+    try:
+        encoded_token = credentials.credentials # <-the token string is in credentials.credentials
+        decoded_token = auth.verify_id_token(encoded_token) # verify and decode the JWT token by calling the firebase admin SDK
+        # print(f"the encoded token is: {encoded_token}") # <- for testing purposes only
+        # print(f"the decoded and verified token is: {decoded_token}") # <- for testing purposes only
+        # print(f"firebase uid is: {decoded_token['uid']}") # <- for testing purposes only
+        return decoded_token
+    except FirebaseError: # <- catch errors raised by firebase admin SDK (invalid/expired/revoked token etc)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Error verifying token",
+        )
