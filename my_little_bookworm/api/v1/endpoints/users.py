451bf3a6f75755d2c088443c9a78a40eb0c5e993
harmel3056 <melh1988@hotmail.com>
2026-02-10 14:00:29 +1100

    Update with dashboard router call

--- my_little_bookworm/api/v1/endpoints/users.py
+++ my_little_bookworm/api/v1/endpoints/users.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from my_little_bookworm.api.v1.schemas.users import CreateUser, UserResponse
+from my_little_bookworm.api.v1.schemas.users import CreateUser, UserResponse, DashboardResponse
 from my_little_bookworm.services.facade import MLBFacade
 from my_little_bookworm.persistence.user_repository import UserRepository
 
@@ -14,8 +14,11 @@ def get_users():
     # Sanity check that the GET endpoint works
     return {'Hello users!'}
 
-
 @router.post("/users", response_model=UserResponse)
 def create_user(req: CreateUser):
     return service.create_user(req)
     # return UserResponse(id=user.id, name=user.name, email=user.email, role=user.role)
+
+@router.get("/users/{user_id}/dashboard", response_model=DashboardResponse)
+def get_dashboard(user_id: str):
+    return service.get_dashboard(user_id)
