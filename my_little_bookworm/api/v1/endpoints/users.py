8cfe711fc51072a08bb2ad88bd4248bfcb344936
harmel3056 <melh1988@hotmail.com>
2026-02-10 12:17:40 +1100

    Updated in alignment with Kat's route.py file

---
+++ my_little_bookworm/api/v1/endpoints/users.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException
+from my_little_bookworm.api.v1.schemas.users import CreateUser, UserResponse
+from my_little_bookworm.services.facade import MLBFacade
+from my_little_bookworm.persistence.user_repository import UserRepository
+
+
+router = APIRouter()
+repo = UserRepository()
+service = MLBFacade(repo)
+
+
+@router.get("/users")
+def get_users():
+    # Sanity check that the GET endpoint works
+    return {'Hello users!'}
+
+
+@router.post("/users", response_model=UserResponse)
+def create_user(req: CreateUser):
+    return service.create_user(req)
+    # return UserResponse(id=user.id, name=user.name, email=user.email, role=user.role)
