fda0abed1996c8b11a23a12195af96ed2a3850ee
harmel3056 <melh1988@hotmail.com>
2026-02-10 14:22:28 +1100

    Base class for common elements of all models

---
+++ my_little_bookworm/domain/base.py
@@ -0,0 +1,19 @@
+import uuid
+from datetime import datetime
+
+"""
+Base class for the common elements of all models
+- uuid
+- created_at
+- updated_at
+""" 
+
+class Base:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    # Allows every model to update the updated_at timestamp
+    def touch(self):
+        self.updated_at = datetime.now()
