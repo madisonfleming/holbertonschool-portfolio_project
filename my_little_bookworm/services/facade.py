10133d39e0701e211e1aeb147cd0a993e7040e47
harmel3056 <melh1988@hotmail.com>
2026-02-11 11:32:50 +1100

    Expand facade class to include all repositories

--- my_little_bookworm/services/facade.py
+++ my_little_bookworm/services/facade.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 
-from app.domain.user import User
-from app.persistence.user_repository import UserRepository
-from app.api.schemas.users import CreateUser
+from my_little_bookworm.domain.user import User
+from my_little_bookworm.persistence.user_repository import UserRepository
+from my_little_bookworm.api.v1.schemas.users import CreateUser
 from typing import Dict
 
 
 class MLBFacade:
-    def __init__(self, repo: UserRepository):
-        self.user_repo = repo
+    def __init__(
+        self,
+        user_repo: UserRepository,
+        child_repo,
+        session_repo,
+        milestone_repo,
+        completion_repo
+    ):
+        self.user_repo = user_repo
+        self.child_repo = child_repo
+        self.session_repo = session_repo
+        self.milestone_repo = milestone_repo
+        self.completion_repo = completion_repo
+
 
     # User
     def create_user(self, req: CreateUser) -> Dict:
