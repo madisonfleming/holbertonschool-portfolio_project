f557f4736e96f36aa982fb1c0e96f070c9b9fbf1
harmel3056 <melh1988@hotmail.com>
2026-02-11 11:08:40 +1100

    Routers established based on resources in the domain

--- my_little_bookworm/api/v1/endpoints/users.py
+++ my_little_bookworm/api/v1/endpoints/users.py
@@ -12,14 +12,15 @@ def get_users():
     return {'Hello users!'}
 
 @router.post("/users", response_model=UserResponse)
-def create_user(req: CreateUser):
-    return service.create_user(req)
+def create_user(req: CreateUser, facade: MLBFacade = Depends(get_facade)):
+    return facade.create_user(req)
     # return UserResponse(id=user.id, name=user.name, email=user.email, role=user.role)
 
 # @router.get("/users/{user_id}/dashboard", response_model=DashboardResponse)
 # def get_dashboard(user_id: str):
 #     return service.get_dashboard(user_id)
 """
+To do:
 DashboardResponse schema to be entered in users.py
 get_dashboard method to be entered in facade.py
 uncomment above router
