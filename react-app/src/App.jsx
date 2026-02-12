9c5ed60be90fc0115698880b3a7a4c509c0df814
andreamgrs <andrea.munozgr@gmail.com>
2026-02-12 23:17:33 +1100

    Adding react-app

---
+++ react-app/src/App.jsx
@@ -0,0 +1,32 @@
+import Login from "./components/auth/login";
+import Header from "./components/header";
+import Dashboard from "./components/dashboard";
+import Home from "./components/home";
+
+import { AuthProvider } from "./contexts/AuthContext";
+//function of react to a list of routes into elements react, browser create the navigation system and navigate only tells where
+import { BrowserRouter, useRoutes } from "react-router-dom";
+
+function AppRoutes() {
+  const routesArray = [
+    { path: "/", element: <Home /> },
+    { path: "*", element: <Home /> },
+    { path: "/login", element: <Login /> },
+    { path: "/dashboard", element: <Dashboard /> },
+    { path: "/home", element: <Home /> },
+  ];
+
+  return useRoutes(routesArray);
+}
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Header />
+          <AppRoutes />
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import MyNavbar from './navbar'
+
+const Header = () => {
+    return (
+        <>
+        <MyNavbar />
+        </>
+    )
+}
+
+export default Header
