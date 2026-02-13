50d1b4ca1f17c0e0e98b323b585e7e444094343f
Mel Harkin <your-email@example.com>
2026-02-13 13:43:54 +1100

    Pathway correction

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -17,10 +17,10 @@ def create_user(req: CreateUser, facade: MLBFacade = Depends(get_facade)):
     return facade.create_user(req)
     # return UserResponse(id=user.id, name=user.name, email=user.email, role=user.role)
 
-@router.get("/users/{user_id}/dashboard", response_model=DashboardResponse)
-def get_dashboard(user_id: str, decoded_token: dict = Depends(auth_current_user)): #<- protected
-    fb_uid = decoded_token["uid"]
-    return service.get_dashboard(user_id, fb_uid)
+# @router.get("/users/{user_id}/dashboard", response_model=DashboardResponse)
+# def get_dashboard(user_id: str, decoded_token: dict = Depends(auth_current_user)): #<- protected
+#     fb_uid = decoded_token["uid"]
+#     return service.get_dashboard(user_id, fb_uid)
 """
 To do:
 DashboardResponse schema to be entered in users.py
