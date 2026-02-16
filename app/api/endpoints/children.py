e0f5489d35ba48aed9984e31b5ff24dbd2e9c00a
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-02-16 04:37:17 +0000

    feat(endpoint): added endpoint to update a child. also added ChildResponse to create child endpoint per FE request

--- app/api/endpoints/children.py
+++ app/api/endpoints/children.py
@@ -4,18 +4,18 @@ from app.api.dependencies import get_facade
 from app.api.auth_dependencies import auth_current_user
 from app.api.schemas.children import CreateChild, ChildResponse, UpdateChild
 
-# TODO: if fb uid not needed to pass to facade - protect all endpoints at the router level
+# TODO: if fb uid not needed to pass to facade - can protect all endpoints at the router level
 router = APIRouter() # auth applied at individual endpoint level. 
 
 # Requirement: Create Child
-@router.post("/children", status_code=201) # TODO: status code returns on success, errors to be handled in new error handling logic. add response_model=ChildResponse if needed
+@router.post("/children", response_model=ChildResponse, status_code=201) # errors to be handled in new error handlling logic
 def create_child(
     child_data: CreateChild,
     facade: MLBFacade = Depends(get_facade),
     decoded_token: dict = Depends(auth_current_user)
     ):
     firebase_uid = decoded_token['uid']
-    return facade.create_child(child_data, firebase_uid) #TODO: use actual facade method name once created, check if firebase_uid is required in facade, remove if not
+    return facade.create_child(child_data, firebase_uid) #TODO: check method name and params match facade
 
 # Requirement: Retrieve a single child
 @router.get("/children/{child_id}", response_model=ChildResponse, status_code=200)
@@ -25,8 +25,18 @@ def get_child(
     decoded_token: dict = Depends(auth_current_user)
     ):
     firebase_uid = decoded_token['uid']
-    return facade.get_child(child_id, firebase_uid) #TODO: check mathod name and params match facade
+    return facade.get_child(child_id, firebase_uid) #TODO: check method name and params match facade
 
+# Requirement: Update a child
+@router.put("/children/{child_id}", response_model=ChildResponse, status_code=200)
+def update_child(
+    child_id: str,
+    updated_child_data: UpdateChild,
+    facade: MLBFacade = Depends(get_facade),
+    decoded_token: dict = Depends(auth_current_user)
+    ):
+    firebase_uid = decoded_token['uid']
+    return facade.update_child(child_id, updated_child_data, firebase_uid) #TODO: check method name and params match facade
 
 
 
