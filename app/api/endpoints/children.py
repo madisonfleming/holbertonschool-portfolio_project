e8851063e83367f229746aa266165a627205f1b7
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-02-16 02:18:35 +0000

    feat(endpoint): add create child endpoint and modified children schema

--- app/api/endpoints/children.py
+++ app/api/endpoints/children.py
@@ -2,16 +2,28 @@ from fastapi import APIRouter, Depends
 from app.services.facade import MLBFacade
 from app.api.dependencies import get_facade
 from app.api.auth_dependencies import auth_current_user
+from app.api.schemas.children import CreateChild, ChildResponse, UpdateChild
 
-router = APIRouter(dependencies=[Depends(auth_current_user)]) #<- attaches auth dependency to all routes in this file
+# TODO: if fb uid not needed to pass to facade - protect all endpoints at the router level
+router = APIRouter() # auth applied at individual endpoint level. 
 
-@router.post("/children")
-def create_child_placeholder(facade: MLBFacade = Depends(get_facade)):
-    return {"status": "ok"}
+# Requirement: Create Child
+@router.post("/children", status_code=201) # TODO: status code returns on success, errors to be handled in new error handling logic. add response_model=ChildResponse if needed
+def create_child(
+    req: CreateChild,
+    facade: MLBFacade = Depends(get_facade),
+    decoded_token: dict = Depends(auth_current_user)
+    ):
+    firebase_uid = decoded_token['uid']
+    return facade.create_child(firebase_uid, req) #TODO: use actual facade method name once created, check if firebase_uid is required in facade, remove if not
 
-@router.get("/children")
-def children_placeholder(facade: MLBFacade = Depends(get_facade)):
-    return {"status": "ok"}
+
+
+
+
+# @router.get("/children")
+# def children_placeholder(facade: MLBFacade = Depends(get_facade)):
+#     return {"status": "ok"}
 
 # placeholder for the momo
 # FastAPI injects the shared facade instance created in app.py via Depends(get_facade)
--- app/api/schemas/children.py
+++ app/api/schemas/children.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel
+from datetime import date
 
 class CreateChild(BaseModel):
     name: str
+    date_of_birth: date
     avatar_url: str | None = None
 
 class ChildResponse(BaseModel):
     child_id: str
     name: str
+    age: int
     avatar_url: str | None = None
 
 class UpdateChild(BaseModel):
     name: str | None = None
+    date_of_birth: date | None = None
     avatar_url: str | None = None
