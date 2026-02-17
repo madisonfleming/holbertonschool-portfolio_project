8282af3710e6349e06072703ddd0b6d778050799
andreamgrs <andrea.munozgr@gmail.com>
2026-02-17 03:58:20 +0000

    Adding reading session card

--- react-app/src/components/dashboard/AddReadingSession.jsx
+++ react-app/src/components/dashboard/AddReadingSession.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 
 const AddReadingSession = (props) => {
+  const child_list = props.children_RS || [];
   const books = [
     "The Very Hungry Caterpillar",
     "Where the Wild Things Are",
@@ -19,6 +20,7 @@ const AddReadingSession = (props) => {
 
   const [selectedBook, setSelectedBook] = useState("");
   const [date, setDate] = useState("");
+  const [selectedChild, setSelectedChild] = useState(null);
 
 
 
@@ -27,7 +29,21 @@ const AddReadingSession = (props) => {
     <div className="popup-overlay">
       <div className="AddReadingSessionCard">
         <h1>Log a reading session</h1>
+        {/* --- USER BUTTONS --- */}
         <h2>Bookworm:</h2>
+        <div className="child-buttons">
+          {child_list.map((child) => (
+            <button
+              key={child.id}
+              className={`child-btn ${selectedChild === child.id ? "active" : ""}`}
+              onClick={() => setSelectedChild(child.id)}
+            >
+              {child.name}
+            </button>
+          ))}
+        </div>
+
+        {/* --- BOOK SELECTOR --- */}
         <h4>Search for your book</h4>
         <select className="select-field" value={selectedBook} onChange={(e) => setSelectedBook(e.target.value)}>
           <option value="">Search...</option>
