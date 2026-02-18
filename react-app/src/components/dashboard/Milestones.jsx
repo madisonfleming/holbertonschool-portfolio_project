ca4cfe5a04d450e19cff2bf47420276d1a5c0b01
madisonfleming <maddyyfleming@gmail.com>
2026-02-18 00:57:35 +0000

    Reading session Button - Sticky. ChildCard now includes GetWorm

--- react-app/src/components/dashboard/Milestones.jsx
+++ react-app/src/components/dashboard/Milestones.jsx
@@ -1,5 +1,5 @@
 
-import GetWorm from "./GetWorm";
+
 import "./Milestones.css"
 const Milestones = ({ current_num_of_books, target }) => {
   const percentage = Math.min((current_num_of_books / 1000) * 100, 100);
@@ -10,9 +10,6 @@ const Milestones = ({ current_num_of_books, target }) => {
       <p className="worm-text">
         {Math.round(percentage)}% of **Billie's** target achieved! That's {current_num_of_books} of {target} books.
       </p>
-      <div>
-        <GetWorm />
-      </div>
     </div>
   )
 
