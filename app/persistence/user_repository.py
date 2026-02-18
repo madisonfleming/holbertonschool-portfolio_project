91e4953e6b4e84f536e79cda32f2bdb59b8df365
Katherine Beringer <github@katbee.au>
2026-02-18 05:51:46 +0000

    feat: Add hardcoded User data to the in-memory persistence layer

--- app/persistence/user_repository.py
+++ app/persistence/user_repository.py
@@ -1,11 +1,55 @@
 #!/usr/bin/python3
-from typing import Dict
 from app.persistence.repository import Repository
 
+""" 
+Set up hardcoded User data for us to use
+while the repository is in-memory
+
+TO DO: remove hardcoded data after MySQL is connected
+"""
+
+user1 = {
+    "created_at": "2026-02-18 04:39:42.220228",
+    "updated_at": "2026-02-18 04:39:42.220228",
+    "id": "a686c824-25e6-4704-87a6-651938429111",
+    "name": "Mary",
+    "email": "mary@example.com",
+    "role": "standard",
+    "firebase_uid": "123",
+    }
+
+user2 = {
+    "created_at": "2026-02-18 04:39:42.220228",
+    "updated_at": "2026-02-18 04:39:42.220228",
+    "id": "b686c824-25e6-4704-87a6-651938429111",
+    "name": "John",
+    "email": "john@example.com",
+    "role": "standard",
+    "firebase_uid": "456",
+    }
+
+user3 = {
+    "created_at": "2026-02-18 04:39:42.220228",
+    "updated_at": "2026-02-18 04:39:42.220228",
+    "id": "c686c824-25e6-4704-87a6-651938429111",
+    "name": "Charlotte",
+    "email": "charlotte@example.com",
+    "role": "standard",
+    "firebase_uid": "789",
+    }
+
+USERS = {
+    user1["id"]: user1,
+    user2["id"]: user2,
+    user3["id"]: user3,
+}
+
+# Uncomment to inspect the User data
+# print(USERS)
 
 class UserRepository(Repository):
     def __init__(self):
-        self._storage = {}
+        self._storage = USERS
 
     def save(self, obj):
         self._storage[obj.id] = obj
