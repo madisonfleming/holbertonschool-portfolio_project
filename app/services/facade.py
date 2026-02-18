ee523e3fb49f51b24bd1f7a768985c5f6261193a
Mel Harkin <your-email@example.com>
2026-02-18 11:23:50 +1100

    WIP child methods

--- app/services/facade.py
+++ app/services/facade.py
@@ -1,30 +1,105 @@
 #!/usr/bin/python3
 
 from app.domain.user import User
+from app.domain.child import Child
+
 from app.persistence.user_repository import UserRepository
-from app.api.schemas.users import CreateUser
-from typing import Dict
+from app.persistence.child_repository import ChildRepository
+from app.persistence.reading_session_repository import ReadingSessionRepository
+from app.persistence.milestone_repository import MilestoneRepository
+from app.persistence.milestone_completion_repository import MilestoneCompletionRepository
+from app.persistence.relationship_repository import RelationshipRepository #----->UPDATE this when join method completed
+
+#from app.api.schemas.users import CreateUser
+from app.api.schemas.children import CreateChild, ChildResponse
+
+from app.domain.exceptions import InvalidChildNameError 
+#from app.services.exceptions import
+
 
 
 class MLBFacade:
     def __init__(
         self,
-        user_repo: UserRepository,
-        child_repo,
-        session_repo,
-        milestone_repo,
-        completion_repo
+        user_repository: UserRepository,
+        child_repository: ChildRepository,
+        reading_session_repository: ReadingSessionRepository,
+        milestone_repository: MilestoneRepository,
+        milestone_completion_repository: MilestoneCompletionRepository,
+        relationship_repository: RelationshipRepository #----->UPDATE this when join method completed
     ):
-        self.user_repo = user_repo
-        self.child_repo = child_repo
-        self.session_repo = session_repo
-        self.milestone_repo = milestone_repo
-        self.completion_repo = completion_repo
+        self.user_repository = user_repository
+        self.child_repository = child_repository
+        self.reading_session_repository = reading_session_repository
+        self.milestone_repository = milestone_repository
+        self.milestone_completion_repository = milestone_completion_repository
+        self.relationship_repository = relationship_repository #----->UPDATE this when join method completed
 
 
-    # User
-    def create_user(self, req: CreateUser) -> Dict:
-        """ create a new user (POST /users)"""
-        user = User(name=req.name, email=req.email)
-        self.user_repo.add(user)
-        return user.to_dict()
+
+    # <--- CHILD --->
+    
+    def create_child(
+        self,
+        request: CreateChild,
+        firebase_uid: str
+    ):
+        if not request.name.strip():    # validate request
+            raise InvalidChildNameError("Child name must not be empty")
+        
+        # create domain model
+        child = Child(
+            name=request.name,
+            date_of_birth=request.date_of_birth
+        )
+        
+        # save domain model to db
+        saved_child = self.child_repository.save(child)
+        
+        # ----->TO DO: get_by_firebase_uid to user_repository
+        # access user's ID via firebase ID
+        user = self.user_repository.get_by_firebase_uid(firebase_uid)
+        user_id = user.id
+
+        # example creation of familyMember join
+        # ----->assumes existence of relationship_repo w/ add_member method
+        self.relationship_repository.add_member(
+            user_id=user_id,
+            child_id=saved_child.id,
+            role="parent" # assuming parent default given they are creating child
+        )
+
+        return ChildResponse.from_domain(saved_child)   # convert domain model to response schema
+
+    def get_children(self, firebase_uid: str):
+        
+        # ----->TO DO: get_by_firebase_uid to user_repository
+        # access user's ID via firebase ID
+        user = self.user_repository.get_by_firebase_uid(firebase_uid)
+        if user is None:
+            raise UserNotFoundError()
+        user_id = user.id
+
+        # fetch/retrieve linked children
+        children = self._get_linked_children(user_id)
+
+        return [ChildResponse.from_domain(child) for child in children]
+
+    def get_child(self):
+        pass
+
+    def update_child(self):
+        pass
+    
+
+    # <--- HELPER FUNCTIONS --->
+    def _get_linked_children(self, user_id: str):
+        # ----->assumes existence of relationship_repo w/ get_children_per_user method
+        # produces list of relationship entries where user_id is parent (or other)
+        relationships = self.relationship_repository.get_children_per_user(user_id)
+        # pull out child IDs from those relationships
+        child_ids = [match.child_id for match in relationships]
+        # return list of child entries from child repo where IDs match
+        return self.child_repository.get_by_ids(child_ids)
+
+
