fe24bbdbf9a8a5ace0cefdbb0a119afa1df7a68c
madisonfleming <maddyyfleming@gmail.com>
2026-02-18 00:57:35 +0000

    Reading session Button - Sticky. ChildCard now includes GetWorm

--- react-app/src/components/dashboard/ChildCard.jsx
+++ react-app/src/components/dashboard/ChildCard.jsx
@@ -1,5 +1,6 @@
 import "./childCard.css"
 import { Link } from 'react-router-dom';
+import GetWorm from "./GetWorm";
 
 /*We dont need to pass the array of kids just using the props to card receives props and shows them */
 const ChildCard = ({ name, age, avatar, lastReadBook }) => {
@@ -12,7 +13,9 @@ const ChildCard = ({ name, age, avatar, lastReadBook }) => {
         <h2 className="child-age">Age: {age}</h2>
         <p className="last-read-book">Last Read Book: {lastReadBook}</p>
       </div>
-      <img className="worm" src="/worm.svg"></img>
+      <div className="worm">
+      <GetWorm />
+      </div>
     </div>
   )
 }
