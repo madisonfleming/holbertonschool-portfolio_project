6cfef2314b93a55130f1d2cf276996eb9cc00259
madisonfleming <maddyyfleming@gmail.com>
2026-02-19 07:02:20 +0000

    Child Selector is working!

--- react-app/src/components/child-profiles/ChildSelector.jsx
+++ react-app/src/components/child-profiles/ChildSelector.jsx
@@ -1,22 +1,74 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import './ChildSelector.css'
 
-const ChildSelector = ({ children_RS, selectedChildId, setSelectedChildId }) => {
+const ChildSelector = ({ children_RS, onSelect }) => {
     const child_list = children_RS || [];
-    /* using selectedChildId so that parent child-profile get selected id */
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedChildId, setSelectedChildId] = useState(null);
+    const dropdownRef = useRef(null);
+
+    const handleSelect = (child) => {
+      setSelectedChildId(child);
+      setIsOpen(false);
+//send child id to parent
+      if (onSelect) {
+        onSelect(child.id);
+      }
+    };
+//if user clicks outside, dropdown closes
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+          setIsOpen(false);
+        }
+      };
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => document.removeEventListener("mousedown", handleClickOutside)
+    }, []);
 
     return (
+      <div className="select-container" ref={dropdownRef}>
+        <div className="dropdown-header"
+        onClick={() => setIsOpen(!isOpen)}>
+          {selectedChildId ? selectedChildId.name : "Wormies"}
+          <img src="/arrow.svg" alt="arrow" className="arrow"></img>
+        </div>
+        {isOpen && (
+          <div className="dropdown-list">
+            {child_list.map((child) => (
+              <div key={child.id}
+              className="dropdown-item"
+              onClick={() => handleSelect(child)}>
+                {child.name}
+                </div>
+            ))}
+          </div>
+          
+        )}
+
+        </div>
+    );
+  }
+  export default ChildSelector;
+
+
+
+
+    /* Child selector half working as select dropdown. styling was impossible
+  
+    return (
         <div>
-          <div className="child-selector">
-            <select className="child-dropdown"
+          <div className="select-container"> {/* select-container */
+            /*<select 
+            id="select"
+            className="child-dropdown"
             value={selectedChildId || ""}
             onChange={(e) => setSelectedChildId(Number(e.target.value))} >
-                {/*<option value="" disabled>Wormies</option>*/}
+                <option value="" disabled>Wormies</option>
           {child_list.map((child) => (
             <option
               key={child.id}
               value={child.id}
-              className={`child-btn ${selectedChildId === child.id ? "active" : ""}`}
-              onClick={() => setSelectedChildId(child.id)}
             >
               {child.name}
             </option>
@@ -26,8 +78,7 @@ const ChildSelector = ({ children_RS, selectedChildId, setSelectedChildId }) => 
         </div>
     )
 }
-export default ChildSelector
-
+*/
 /* child selector working as buttons
 return (
         <div>
