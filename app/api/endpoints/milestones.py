563f3da337c55cf07086f44eb657203ce1b2d92a
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-02-19 04:34:40 +0000

    feat(endpoint): added milestones endpoints and updated milestones schema

--- app/api/endpoints/milestones.py
+++ app/api/endpoints/milestones.py
@@ -2,12 +2,29 @@ from fastapi import APIRouter, Depends
 from app.services.facade import MLBFacade
 from app.api.dependencies import get_facade
 from app.api.auth_dependencies import auth_current_user
+from app.api.schemas.milestones import MilestoneResponse
+from typing import List
 
-router = APIRouter(dependencies=[Depends(auth_current_user)]) #<- attaches auth dependency to all routes in this file
+# TODO: if fb uid/user_id not needed to pass to facade - can protect all endpoints at the router level with: router = APIRouter(dependencies=[Depends(auth_current_user)])
+router = APIRouter() # NOTE: auth applied at individual endpoint level for now. 
 
-@router.get("/milestones")
-def milestones_placeholder(facade: MLBFacade = Depends(get_facade)):
-    return {"status": "ok"}
-# placeholder for the momo
-# FastAPI injects the shared facade instance created in app.py via Depends(get_facade)
-# endpoints/reading_sessions.py has a mental model check FYI
+# Requirement: Get ALL milestones
+@router.get("/children/{child_id}/milestones", response_model=List[MilestoneResponse], status_code=200)
+def get_all_milestones(
+    child_id: str,
+    facade: MLBFacade = Depends(get_facade),
+    decoded_token: dict = Depends(auth_current_user)
+    ):
+    firebase_uid = decoded_token["uid"]
+    return facade.get_all_milestones(child_id, firebase_uid) #TODO: Check method name and params match facade
+
+# Requirement: Get ONE milestone
+@router.get("/children/{child_id}/milestones/{milestone_id}", response_model=MilestoneResponse, status_code=200)
+def get_one_milestone(
+    child_id: str,
+    milestone_id: str,
+    facade: MLBFacade = Depends(get_facade),
+    decoded_token: dict = Depends(auth_current_user)
+    ):
+    firebase_uid = decoded_token["uid"]
+    return facade.get_one_milestone(child_id, milestone_id, firebase_uid) #TODO: Check method name and params match facade
