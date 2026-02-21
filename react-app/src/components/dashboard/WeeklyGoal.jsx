44264c04ccf500f98ce807a1d5a74c66861087de
andreamgrs <andrea.munozgr@gmail.com>
2026-02-21 09:19:52 +0000

    Fixing dashboard and log a reading session card

--- react-app/src/components/dashboard/WeeklyGoal.jsx
+++ react-app/src/components/dashboard/WeeklyGoal.jsx
@@ -1,14 +1,18 @@
-import "./WeeklyGoal.css"
+import "./WeeklyGoal.css";
 const WeeklyGoal = ({ theme, current_num_of_books, target }) => {
   return (
-    <div className="weekly-goal-card" >
+    <div className="weekly-goal-card">
       <h2>It's {theme} Week!</h2>
       <img src={`/themes/${theme}.png`} alt={theme} className="theme-img" />
-      <h3>Read {target} books about <strong>{theme}</strong> to earn the reward!</h3>
-      <h3> Progress: {current_num_of_books} / {target} </h3>
+      <h3>Target: </h3>
+      <h3>
+        Read {target} books about <strong>{theme}</strong> this week to earn the
+        reward!
+      </h3>
+      <h3> Finished? Click in complete </h3>
+      <button>Complete</button>
     </div>
+  );
+};
 
-  )
-}
-
-export default WeeklyGoal
+export default WeeklyGoal;
