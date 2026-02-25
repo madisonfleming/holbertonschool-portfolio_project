6ef7bab092afbd5af683709e8031dedb851fb45d
Mel Harkin <your-email@example.com>
2026-02-25 17:11:46 +1100

    Update nonetype to 'nullable and optional' state rather than existing required state

--- app/api/schemas/books.py
+++ app/api/schemas/books.py
@@ -1,15 +1,17 @@
+from pydantic import BaseModel
+from datetime import date
+
 class BookSearchResponse(BaseModel):
-    book_id: str | None      # optional None if external lib only
+    book_id: str | None = None      # optional None if external lib only
     external_id: str
     source: str         # openLibrary 
     title: str
-    author: str | None
-    description: str | None
-    cover_url: str | None
+    author: str | None = None
+    cover_url: str | None = None
 
 class BookResponse(BaseModel):
     book_id: str
     title: str
     author: str
-    description: str | None
-    cover_url: str | None
+    description: str | None = None
+    cover_url: str | None = None
