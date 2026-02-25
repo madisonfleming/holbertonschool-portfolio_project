6451df5694d8dfacdfaf10dd9c3dd6fc9297b826
Mel Harkin <your-email@example.com>
2026-02-25 17:27:32 +1100

    Update subjects types as FastAPI treats list params as body and it messes up the swagger testing. Just tells it that it is a query param not body

--- app/api/endpoints/books.py
+++ app/api/endpoints/books.py
@@ -1,20 +1,23 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from app.services.facade import MLBFacade
 from app.api.dependencies import get_facade
 from app.api.auth_dependencies import auth_current_user
 from app.api.schemas.books import BookSearchResponse, BookResponse
 from typing import List
 
+router = APIRouter()
+
 # book search function
 @router.get("/books/search", response_model=List[BookSearchResponse], status_code=200)
 def search_books(
-    query: str,
-    subject: List[str] | None = None
+    q: str, #query
+    subjects: List[str] | None = Query(None),
+    limit: int | None = None,
     facade: MLBFacade = Depends(get_facade),
-    decoded_token: dict = Depends(auth_current_user)
+    # decoded_token: dict = Depends(auth_current_user)
 ):
-    firebase_uid = decoded_token['uid']
-    return facade.search_books(query, subject, firebase_uid)
+    # firebase_uid = decoded_token['uid']
+    return facade.search_books(q, subjects) #TODO return firebase_uid once testing complete
 
 # get book by ID (to return book deets to dashy b)
 # this method should offer greater stability for timeline than accessing ext API every time
