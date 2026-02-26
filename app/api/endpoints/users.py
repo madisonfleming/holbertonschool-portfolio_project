e38d26a198f47be89561620058a16e5b9c9b6b78
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-02-26 05:23:12 +0000

    feat(users): updated BE end to end for get user and update user

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -7,19 +7,19 @@ from app.config import get_settings_class
 from fastapi import HTTPException
 
 router = APIRouter()
-Settings = get_settings_class() # get config class in use
-settings = Settings() # create instance of class to check if app in dev mode
 
-# Requirement: Create User
-# NOTE: ENDPOINT RESTRICTED TO DEVS ONLY
-@router.post("/users", response_model=UserResponse, status_code=201)
-def create_user(
-    user_data: CreateUser,
-    facade: MLBFacade = Depends(get_facade)
-    ):
-    if settings.DEBUG:
-        return facade.create_user(user_data)
-    raise HTTPException(status_code=403)
+# # Requirement: Create User
+# # NOTE: ENDPOINT RESTRICTED TO DEVS ONLY. THIS ENDPOINT SAVES TO REPO BY USER ID NOT FB UID - NOT TO BE USED IN PRODUCTION
+# Settings = get_settings_class() # get config class in use
+# settings = Settings() # create instance of class to check if app in dev mode
+# @router.post("/users", response_model=UserResponse, status_code=201)
+# def create_user(
+#     user_data: CreateUser,
+#     facade: MLBFacade = Depends(get_facade)
+#     ):
+#     if settings.DEBUG:
+#         return facade.create_user(user_data)
+#     raise HTTPException(status_code=403)
 
 # Requirement: Retrieve a User
 @router.get("/users/me", response_model=UserResponse, status_code=200)
@@ -28,7 +28,7 @@ def get_user(
     decoded_token: dict = Depends(auth_current_user)
     ):
     firebase_uid = decoded_token["uid"]
-    return facade.get_user(firebase_uid) #TODO: check method and params match facade
+    return facade.get_user(firebase_uid)
 
 # Requirement: Update a User
 @router.put("/users/me", response_model=UserResponse, status_code=200)
@@ -38,4 +38,4 @@ def update_user(
     decoded_token: dict = Depends(auth_current_user)
     ):
     firebase_uid = decoded_token["uid"]
-    return facade.update_user(updated_data, firebase_uid) #TODO: check method and params match facade
+    return facade.update_user(updated_data, firebase_uid)
