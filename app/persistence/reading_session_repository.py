f35d81e420a77973e07d068f4b3bcff36717a319
Mel Harkin <your-email@example.com>
2026-02-27 16:42:37 +1100

    Implement save and update methods

--- app/persistence/reading_session_repository.py
+++ app/persistence/reading_session_repository.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 
 from app.persistence.repository import Repository
+from app.domain.reading_session import ReadingSession
+
+from datetime import datetime
+from uuid import uuid4
 
 
 class ReadingSessionRepository(Repository):
     def __init__(self):
         self._storage = {}
 
-    def save(self, session):
-        self._storage[session.id] = session
+    def save(
+        self,
+        child_id: str,
+        book_id: str,
+        logged_at: datetime
+    ):
+        session = ReadingSession(
+            session_id=str(uuid4()),
+            child_id=child_id,
+            book_id=book_id,
+            logged_at=logged_at
+        )
+        self._storage[session.session_id] = session
+        return session
 
-    def get(self, session_id):
+    def get_by_id(self, session_id: str):
         return self._storage.get(session_id)
 
-    def get_by_child(self, child_id):
+    def get_by_child(self, child_id: str):
         return [
             session for session in self._storage.values()
             if session.child_id == child_id
@@ -22,11 +38,9 @@ class ReadingSessionRepository(Repository):
     def get_all(self):
         return []
 
-    def get_by_attribute(self, attr, value):
-        return None
-
-    def update(self, id, data):
-        pass
+    def update(self, session: ReadingSession):
+        self._storage[session.session_id] = session
+        return session
 
     def delete(self, id):
         pass
