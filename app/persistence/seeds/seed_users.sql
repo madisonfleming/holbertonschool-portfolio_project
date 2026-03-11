d11792be1f89c5f8884fea4a281b145dc0051269
Katherine Beringer <github@katbee.au>
2026-03-11 08:00:47 +0000

    feat: Add seed sql scripts for user, child, relationship, reading_session and book
    Touch up minor issues in existing seeds

---
+++ app/persistence/seeds/seed_users.sql
@@ -0,0 +1,10 @@
+INSERT INTO User (id, name, email, role, firebase_uid, created_at, updated_at)
+VALUES
+("a686c824-25e6-4704-87a6-651938429111", "Evangeline", "evangeline@example.com", "standard", "CVelQleFzqXvvuLQGllEnP4FnhD2", NOW(), NOW()),
+("a686c824-25e6-4704-87a6-651938429222", "Patrick", "patrick@example.com", "standard", "e81bwYvV0UUoW2R44BQ7zoRv2eH3", NOW(), NOW()),
+("a686c824-25e6-4704-87a6-651938429333", "Uma", "uma@example.com", "standard", "EBE8aoxSz0TXAkIn2rJly2N5aI42", NOW(), NOW()),
+("a686c824-25e6-4704-87a6-651938429444", "Elena", "elena@example.com", "standard", "97AhiAtJKKbtHZs13YmaLgKdKX53", NOW(), NOW()),
+("a686c824-25e6-4704-87a6-651938429555", "Michael", "michael@example.com", "standard", "4CitA4N95BfWnww1Wi4pX23ZhnU2", NOW(), NOW()),
+("a686c824-25e6-4704-87a6-651938429666", "Marian", "mylittlebookworm27@gmail.com", "standard", "tbc", NOW(), NOW());
+
+SELECT 'User seed was an astonishing success' AS status;
