838bff2f26de449157da9fff031d33b1550194f4
Mel Harkin <melh1988@hotmail.com>
2026-03-13 18:34:59 +1100

    Script to run all seeds at once

--- app/persistence/seeds/master_seed.sql
+++ app/persistence/seeds/master_seed.sql
@@ -4,9 +4,9 @@
 -- mysql -h 127.0.0.1 -P 3306 -u root -p my_little_bookworm < master_seed.sql
 
 SOURCE ./seed_books.sql;
+SOURCE ./seed_users.sql;
 SOURCE ./seed_children.sql;
 SOURCE ./seed_milestone_type.sql;
 SOURCE ./seed_milestone_completion.sql;
 SOURCE ./seed_reading_sessions.sql;
 SOURCE ./seed_relationships.sql;
-SOURCE ./seed_users.sql;
