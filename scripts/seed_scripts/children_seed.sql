0517a606934540ae2f4d25e9c6b3d1acc29425cd
Mel Harkin <melh1988@hotmail.com>
2026-03-14 11:21:38 +1100

    Remove duplicate data

--- scripts/seed_scripts/children_seed.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Child (id, name, date_of_birth, avatar_url, created_at, updated_at)
-VALUES
-("e686c824-25e6-4704-87a6-651938429111", "Stella", "2018-01-01", "/avatars/mlb-avatar-sun", NOW(), NOW()),
-("e686c824-25e6-4704-87a6-651938429222", "Betty", "2017-05-12", "/avatars/mlb-avatar-bee", NOW(), NOW()),
-("e686c824-25e6-4704-87a6-651938429333", "Tommy", "2016-09-03", "/avatars/mlb-avatar-robot", NOW(), NOW()),
-("e686c824-25e6-4704-87a6-651938429444", "Billy", "2015-11-20", "/avatars/mlb-avatar-sun", NOW(), NOW());
-SELECT 'Child seed was a massive success' AS status;
