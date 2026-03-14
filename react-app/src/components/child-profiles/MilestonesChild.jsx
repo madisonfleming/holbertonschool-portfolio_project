66c250eb1b947a2c812bedef7cb5fa03d149a959
andreamgrs <andrea.munozgr@gmail.com>
2026-03-14 23:55:17 +0000

    Adding design for milestones progress

--- react-app/src/components/child-profiles/MilestonesChild.jsx
+++ react-app/src/components/child-profiles/MilestonesChild.jsx
@@ -1,28 +1,34 @@
+import "./MilestonesChild.css";
+import ExportButton from "./ExportButton";
+import { useEffect, useState } from "react";
+import { useMilestones } from "../../contexts/MilestonesContext";
 
-import { useChild } from '../../contexts/ChildContext'
-import "./MilestonesChild.css"
-import ExportButton from './ExportButton';
-import CompletedMilestones from './CompletedMilestones';
-import { ClipPath } from '@react-pdf/renderer';
-
-const MilestonesChild = ({ current_num_of_books, target, selectedChild, certificateData, completedMilestones }) => {
+const MilestonesChild = ({
+  current_num_of_books,
+  target,
+  selectedChild,
+  certificateData,
+  completedMilestones,
+}) => {
   const percentage = Math.min((current_num_of_books / 1000) * 100, 100);
 
-
   return (
-    <div className="milestone-card">
-        <p className="milestone-text">{selectedChild.name} is doing great!</p>
-          <div className="body-text">
-            <div className="percentage">{Math.round(percentage)}%</div>
-             of {selectedChild?.name}'s target achieved! That's {current_num_of_books} of {target} books!
-          </div>
-          <div className="export-btn">
-            <ExportButton certificateData={certificateData} selectedChild={selectedChild}/>
-          </div>
+    <div className="milestoneChild-card">
+      <h1 className="card-title"> Reading Progress </h1>
+      <p className="card-subtitle"> {selectedChild.name} is doing great!</p>
+      <div className="body-text">
+        <div className="percentage">{Math.round(percentage)}%</div>
+        of {selectedChild?.name}'s target achieved! That's{" "}
+        {current_num_of_books} of {target} books!
+      </div>
+      <div className="export-btn">
+        <ExportButton
+          certificateData={certificateData}
+          selectedChild={selectedChild}
+        />
+      </div>
+    </div>
+  );
+};
 
-  </div>
-  )
-
-}
-
-export default MilestonesChild
+export default MilestonesChild;
