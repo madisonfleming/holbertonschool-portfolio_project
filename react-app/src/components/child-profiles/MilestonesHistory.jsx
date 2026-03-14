678c807d678ec228f67b52c4b5be3b479b0d85f2
andreamgrs <andrea.munozgr@gmail.com>
2026-03-14 08:28:10 +0000

    Adding all endpoints for milestons all working

--- react-app/src/components/child-profiles/MilestonesHistory.jsx
+++ react-app/src/components/child-profiles/MilestonesHistory.jsx
@@ -1,39 +1,94 @@
-
-import { useChild } from '../../contexts/ChildContext'
-import "./MilestonesHistory.css"
-import ExportButton from './ExportButton';
-import CompletedMilestones from './CompletedMilestones';
-import { ClipPath } from '@react-pdf/renderer';
+import { useChild } from "../../contexts/ChildContext";
+import "./MilestonesHistory.css";
+import ExportButton from "./ExportButton";
+import CompletedMilestones from "./CompletedMilestones";
+import { ClipPath } from "@react-pdf/renderer";
 import { useEffect, useState } from "react";
 import { useMilestones } from "../../contexts/MilestonesContext";
-
 
 const MilestonesHistory = ({ selectedChild }) => {
   //We need to save the milestones in states
   /* 1. Call endpoint inside useEffect. 
   2 Saved Milestones on useState 
   3.Render the state. 
-  4 Send id of selectedChild */ 
+  4 Send id of selectedChild */
   const { allMilestonesPerChild } = useMilestones();
+  const { weeklyGoalMilestonesPerChild } = useMilestones();
+  const { booksReadMilestonesPerChild } = useMilestones();
   const [milestonesPerChild, setMilestonesPerChild] = useState([]);
+  const [getWeeklyGoalMilestonesPerChild, setWeeklyGoalMilestonesPerChild] =
+    useState([]);
+  const [getBooksReadMilestonesPerChild, setBooksReadMilestonesPerChild] =
+    useState([]);
+
+  const icons = {
+    books: '<img src="/open-book.png" />',
+    weekly: '<img src="/open-book.png" />',
+  };
+  const labels = { books: "Books Milestone", weekly: " Weekly Goal" };
 
   useEffect(() => {
     if (!selectedChild?.id) return;
 
-    async function fetchDataFromAllMilestonesPerChild() {
-      const data = await allMilestonesPerChild(selectedChild.id);
-      console.log("DATA FROM BE ON SELECTED CHILD INSIDE MilestonesHistoryjsx:", data);
-      setMilestonesPerChild(data); 
+    //this fetch is to see the milestons per child
+    async function fetchDataFromMilestonesPerChild() {
+      //all milestones
+      const dataAllMilestones = await allMilestonesPerChild(selectedChild.id);
+      console.log(
+        "DataAllMilestones FROM BE ON SELECTED CHILD INSIDE MilestonesHistoryjsx:",
+        dataAllMilestones,
+      );
+      setMilestonesPerChild(dataAllMilestones);
+      //just weekly goal
+      const dataWeeklyGoalMilestones = await weeklyGoalMilestonesPerChild(
+        selectedChild.id,
+      );
+      console.log(
+        "Dataweeklygoalmilestones FROM BE ON SELECTED CHILD INSIDE MilestonesHistoryjsx:",
+        dataWeeklyGoalMilestones,
+      );
+      setWeeklyGoalMilestonesPerChild(dataWeeklyGoalMilestones);
+      //just books read
+      const dataBooksReadMilestones = await booksReadMilestonesPerChild(
+        selectedChild.id,
+      );
+      console.log(
+        "DataReadBooksMilestones FROM BE ON SELECTED CHILD INSIDE MilestonesHistoryjsx:",
+        dataBooksReadMilestones,
+      );
+      setBooksReadMilestonesPerChild(dataBooksReadMilestones);
     }
 
-    fetchDataFromAllMilestonesPerChild();
+    fetchDataFromMilestonesPerChild();
   }, [selectedChild]);
 
   return (
-  <div className="milestoneHistory-card">
-    <h2>Milestones History</h2>
+    <div className="milestoneHistory-card">
+      <h1 className="card-title"> Milestones </h1>
+      <p className="card-subtitle"> {selectedChild.name}'s achievements</p>
+      {/* Filter tabs*/}
+      <div className="filter-row">
+        <button
+          className="filter-tab active"
+          onclick="filterMilestones('books', this)"
+        >
+          Books Read
+        </button>
+        <button class="filter-tab" onclick="filterMilestones('weekly', this)">
+          Weekly Goal
+        </button>
+      </div>
+
+      {/*Timeline*/}
+      <div class="timeline-wrap">
+        <div class="timeline" id="timeline"></div>
+        <div class="empty-state" id="emptyState">
+          <div class="empty-text">No milestones yet!</div>
+        </div>
+      </div>
+      {/* 
       <div className="milestone-badges">
-         {milestonesPerChild.length === 0 ? (
+        {milestonesPerChild.length === 0 ? (
           <p>No milestones yet</p>
         ) : (
           milestonesPerChild.map((data) => (
@@ -43,9 +98,28 @@ const MilestonesHistory = ({ selectedChild }) => {
           ))
         )}
 
-      </div>
-  </div>
-  )
+        {getWeeklyGoalMilestonesPerChild.length === 0 ? (
+          <p>No milestones yet</p>
+        ) : (
+          getWeeklyGoalMilestonesPerChild.map((data) => (
+            <div key={data.id}>
+              <strong>{data.description}</strong>
+            </div>
+          ))
+        )}
 
-}
-export default MilestonesHistory
+        {getBooksReadMilestonesPerChild.length === 0 ? (
+          <p>No milestones yet</p>
+        ) : (
+          getBooksReadMilestonesPerChild.map((data) => (
+            <div key={data.id}>
+              <strong>{data.description}</strong>
+            </div>
+          ))
+        )}
+      </div>
+      */}
+    </div>
+  );
+};
+export default MilestonesHistory;
