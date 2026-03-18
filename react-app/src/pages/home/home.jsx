74d72bcd11d78bbc9de55b3ebf44a643ec52c4e8
madisonfleming <maddyyfleming@gmail.com>
2026-03-18 10:53:42 +0000

    rebase home

--- react-app/src/pages/home/home.jsx
+++ react-app/src/pages/home/home.jsx
@@ -7,10 +7,34 @@ export default function Home() {
   return (
     <div className="home-grid">
     <div className="container1"></div>
-    <div className="container2">
-      <Container2 />
+    <div className="container2"></div>
+    <div className="container3">
+      <div className="container3-top">
+        <div className="container3-top-left">
+        <p>
+          Download a Certificate after
+          completing a Milestone or Reward
+          that you<br /> and your child can hold
+          onto forever  
+        </p>
+        <p>
+          (if you get a star, don't forget to
+          cut and colour with your wormie!)
+        </p>
+        </div>
+        <div className="container3-top-right">
+        <img src={"/certificates.png"} className="certificates" />
+        </div>
+      </div>
+      <div className="container3-bottom">
+      <div className="container3-bottom-left">
+        <img src={"/mlb-worm_badge.svg"} className="worm-badge" /> {/* worm image on bottom left */}
+      </div>
+      <div className="container3-bottom-right">
+        <p>Track your wormies progress<br /> toward the 1,000 book goal</p>
+      </div>
+      </div>
     </div>
-    <div className="container3"></div>
     </div>
   );
 }
