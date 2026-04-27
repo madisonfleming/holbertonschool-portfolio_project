eaedde558171b1c3501c2fc9dab53ec103379dc9
Katherine Beringer <github@katbee.au>
2026-04-27 11:27:02 +1000

    fix: tweak the db setup script to use the parameterised password

--- scripts/setup_script.sh
+++ scripts/setup_script.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/bash
-DB_HOST=host.docker.internal
 
-# For Mel:
-# DB_HOST=127.0.0.1
+# DB_HOST is normally defined in .env. Set a default if unavailable:
+if [[ -z "${DB_HOST}" ]]; then
+    DB_HOST="127.0.0.1"
+fi
 
 # Drop tables if exist
 echo "Dropping tables..." 
-mysql -h $DB_HOST -P 3306 -u root -p my_little_bookworm < app/persistence/seeds/reseed_helper.sql
+MYSQL_PWD="${DB_PASSWORD}" mysql -h $DB_HOST -P 3306 -u root my_little_bookworm < app/persistence/seeds/reseed_helper.sql
 echo "No more tables"
 
 # Create tables
 echo "Creating tables..."
-PYTHONPATH=. python scripts/create_tables.py
+PYTHONPATH=. python3 scripts/create_tables.py
 echo "Tables recreated successfully!"
 
 # Populate tables with seed data
 echo "Seeding dev data..."
-mysql -h $DB_HOST -P 3306 -u root -p my_little_bookworm < app/persistence/seeds/master_seed.sql
+MYSQL_PWD="${DB_PASSWORD}" mysql -h $DB_HOST -P 3306 -u root my_little_bookworm < app/persistence/seeds/master_seed.sql
 echo "🎉 Finished seeding data into my_little_bookworm 🎉"
